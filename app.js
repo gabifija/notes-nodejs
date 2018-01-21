b31b5e5f6312f4c4ff43ac194933049ad5274019
Gabi <gabriela@assembla.com>
2018-01-21 08:44:31 +0100

    Added input from user and lodash module

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 
-var user = os.userInfo();
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'Hello World', function (err) {
-  if (err) {
-    console.log('Unable to write a file');
-  }
-});
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
---
+++ notes.js
@@ -0,0 +1,9 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = () => {
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
